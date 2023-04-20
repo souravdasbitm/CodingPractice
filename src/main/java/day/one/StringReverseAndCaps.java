6365b9d85dda406d2fc3c1045cdef0c017f00daf
Sourav Das <souravdasbitm@gmail.com>
2023-04-20 10:59:46 +0530

    StringReverseAndCaps

--- src/main/java/day/one/StringReverseAndCaps.java
+++ src/main/java/day/one/StringReverseAndCaps.java
@@ -14,15 +14,19 @@ public class StringReverseAndCaps {
 
         //split the string from ""
         String[] strR = String.valueOf(strB).split(" ");
+        strB.delete(0,str.length());
         for (int j = 0; j < strR.length; j++) {
             for (int k = 0; k < strR[j].length(); k++) {
                 if (k == 0) {
-                    System.out.println(j + "--" + k + "---" + strR[j].toUpperCase().charAt(k));
+                   // System.out.println(j + "--" + k + "---" + strR[j].toUpperCase().charAt(k));
+                    strB.append(" " + strR[j].toUpperCase().charAt(k));
                 } else {
-                    System.out.println(j + "--" + k + "---" + strR[j].charAt(k));
+                    //System.out.println(j + "--" + k + "---" + strR[j].charAt(k));
+                    strB.append(strR[j].charAt(k));
                 }
             }
         }
+        System.out.println(String.valueOf(strB).trim());
 
     }
 }
