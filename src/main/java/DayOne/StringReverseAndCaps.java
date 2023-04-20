fcd13d08c1e4fcd44262889e0b43194dbc8efbf4
Sourav Das <souravdasbitm@gmail.com>
2023-04-20 10:13:19 +0530

    StringReverseAndCaps

---
+++ src/main/java/DayOne/StringReverseAndCaps.java
@@ -0,0 +1,28 @@
+package DayOne;
+
+public class StringReverseAndCaps {
+    /*
+        Reverse the string and then do the init caps
+     */
+    public static void main(String[] args) {
+        String str = "hello world";
+        StringBuilder strB = new StringBuilder();
+        // Is to reverse the string and put it in string builder
+        for (int i = str.length() - 1; i >= 0; i--) {
+            strB.append(str.charAt(i));
+        }
+
+        //split the string from ""
+        String[] strR = String.valueOf(strB).split(" ");
+        for (int j = 0; j < strR.length; j++) {
+            for (int k = 0; k < strR[j].length(); k++) {
+                if (k == 0) {
+                    System.out.println(j + "--" + k + "---" + strR[j].toUpperCase().charAt(k));
+                } else {
+                    System.out.println(j + "--" + k + "---" + strR[j].charAt(k));
+                }
+            }
+        }
+
+    }
+}
